c1d4c54bd4f5718a2290356ae24187b718908d09
Sidharth-Chirathazha <sidharthchirathazha@gmail.com>
2025-04-24 19:23:12 +0530

    fix: added a default admin creation

---
+++ postify_backend/users/management/commands/create_default_admin.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Create default admin user if not exists'
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin123').exists():
+            User.objects.create_superuser('admin123', 'admin@example.com', 'Admin@123')
+            self.stdout.write(self.style.SUCCESS('Superuser created'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
